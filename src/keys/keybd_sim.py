6ccd2c4c3e91515e40831a6cf0e9512a5d225960
holychowders <theholychowders@gmail.com>
2021-12-29 13:16:26 -0600

    keybd_sim: Emit neater warning when game window not found.

--- src/keys/keybd_sim.py
+++ src/keys/keybd_sim.py
@@ -2,6 +2,7 @@ from pyautogui import getWindowsWithTitle
 from pydirectinput import press
 
 from time import sleep
+from warnings import warn
 
 
 def press_key(char: str) -> None:
@@ -14,8 +15,9 @@ def _focus_game_window() -> None:
     # FIXME: We must be sure another window with the same title can't get selected
     for window in getWindowsWithTitle('Star Trek Online'):
         if window.title == 'Star Trek Online':
-            game_window = window
+            window.activate()
             break
+    else:
+        warn('Game window not found and will not be focused.', RuntimeWarning)
 
-    game_window.activate()
 
